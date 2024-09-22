2b5e63964869b155127ca2145429fdb3f9a2699a
Kohei Mizokami <mizokami1108work@gmail.com>
2024-09-22 12:34:32 +0900

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input v-model="taskName" />
+    <p>{{ taskName }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const taskName = ref<string>("init");
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nav>ナビゲーションバー</nav>
+    <slot />
+  </div>
+</template>
